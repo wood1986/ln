ea8c082d084e6330e60357654daa284fd86a0ecc
wood </>
2014-06-12 23:50:40 +0800

    fixed benchmark/run.sh to support linux

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -1,9 +1,15 @@
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  args="-v"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  args="-l"
+fi
+
 files=("ln" "bunyan" "log4js" "winston")
 
 for i in "${files[@]}"
 do
   echo $i
   rm -rf "$i.log"
-  /usr/bin/time -l node "$i.js" $1 $2
+  /usr/bin/time $args node "$i.js" $1 $2
   echo "========================="
 done
