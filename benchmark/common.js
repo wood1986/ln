d910a5c49b643f79f8ab37c76dfd159bfab178e7
Ryan Graham <ryan@strongloop.com>
2014-06-18 17:16:32 -0700

    Make benchmark async
    Real apps don't spin in a blocking loop

--- benchmark/common.js
+++ benchmark/common.js
@@ -1,6 +1,12 @@
 /* global log:true */
 var n = process.argv[2] ? parseInt(process.argv[2]) : 100000;
 console.info("log.info(%d) with %d time(s)", process.pid, n);
-for (var i = 0; i < n; i++) {
-  log.info(i);
+var i = 0;
+
+function tick() {
+  // log a "request" each tick
+  log.info(i++);
+  if (i < n) setImmediate(tick);
 }
+
+setImmediate(tick);
