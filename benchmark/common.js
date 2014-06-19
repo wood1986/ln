4e689d849be3a4c3e280dfac19b8bcf130262bce
Ryan Graham <ryan@strongloop.com>
2014-06-18 17:08:30 -0700

    Minor simplification of benchmark common.js

--- benchmark/common.js
+++ benchmark/common.js
@@ -1,7 +1,6 @@
 /* global log:true */
-var util = require("util");
 var n = process.argv[2] ? parseInt(process.argv[2]) : 100000;
-console.log(util.format("log.info(%d) with %d time(s)", process.pid, n));
+console.info("log.info(%d) with %d time(s)", process.pid, n);
 for (var i = 0; i < n; i++) {
   log.info(i);
 }
